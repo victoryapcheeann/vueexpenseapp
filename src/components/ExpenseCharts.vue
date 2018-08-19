dcf77965d04c314993f765b1159798b774aa1c7f
Victor Yap <victoryapcheeann@gmail.com>
2018-08-19 13:53:12 +0800

    Added header style and animation

--- src/components/ExpenseCharts.vue
+++ src/components/ExpenseCharts.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <p class='chartHeader'>My Expense Chart</p>
+      <div class='chartHeader'>Expense Over Time</div>
       <line-chart 
         :data="expenseListForChart" 
         :colors="['orange']"
@@ -30,16 +30,28 @@ export default {
 </script>
 
 <style scoped>
-div {
-  border-radius: 40px;
-  padding: 30px;
-}
-
 .chartStyle {
+  padding: 30px;
+  border-radius: 40px;
   background-image: linear-gradient(-100deg, #a1c4fd 0%, #c2e9fb 100%);
+  box-shadow: 10px 10px 20px -6px rgb(161, 218, 255);
 }
 
 .chartHeader {
-  text-align: center;
+  text-align: left;
+  background-image: linear-gradient(30deg, #d5ff73 0%, #bbeec2 100%);  background-size: 100px 20px;
+  background-repeat: no-repeat;
+  background-position: bottom left;
+  padding: 10px;
+  margin: 10px;
+  width: 300px;
+  font-size: 20px;
+  text-transform: uppercase;  
+  transition: all 1s;
+}
+
+.chartHeader:hover {
+  background-size: 200px 20px; 
+  transition: all 1s;
 }
 </style>
